2fc9659ef714de99beba24f496c6f464e91467e6
hinoob <reio.poldmaa@voco.ee>
2024-09-23 08:48:16 +0300

    ejs paigaldamine

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 
+const path = require("path");
+app.set("view engine", 'ejs');
+app.set("views", path.join(__dirname, 'views'));
+
 app.get("/", function(req, res){
-    res.send("Hello World")
+    res.render('index');
 });
+
 app.listen(3000, () => {
     console.log("Server kuulab pordi peal 3000!")
 });
