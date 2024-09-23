0d0b1da7ca3c4c3396439bcebcccd1d7776a9abd
hinoob <reio.poldmaa@voco.ee>
2024-09-23 09:28:33 +0300

    Created form request responce with API data

--- index.js
+++ index.js
@@ -4,9 +4,29 @@ const app = express();
 const path = require("path");
 app.set("view engine", 'ejs');
 app.set("views", path.join(__dirname, 'views'));
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 require("dotenv").config();
 
 const key = process.env.WEATHER_API_KEY;
+
+app.post("/", function(req, res){
+    console.log("form send data", req.body.cityname);
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${req.body.cityname}&appid=${key}`, {
+        method: "GET"
+    }).then(res => res.json()).then(data => {
+        let desc = data.weather[0].description;
+        let city = data.name;
+        let temp = Math.round(parseFloat(data.main.temp)-273.15)
+        res.render("index", {
+            description: desc,
+            city,
+            temp
+        });
+    })
+});
 
 app.get("/", function(req, res){
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=Tartu&appid=${key}`, {
